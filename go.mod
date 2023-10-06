d964ccfd44bab4e3b65791db623214f5403344ae
tabish-bp <tabish@bitspro.com>
2023-10-06 16:52:29 +0500

    everything done

---
+++ go.mod
@@ -0,0 +1,41 @@
+module github.com/tabish-bp/go_db_app
+
+go 1.21.1
+
+require (
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/joho/godotenv v1.5.1
+	gormoi//ddiver/postgres v0.2.4
+	gorm.io/gorm v1.25.4
+)
+
+require (
+	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/goidentity/v6 v6.0.1 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/lib/pq v1.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/tinylib/msgp v1.1.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.44.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/net v0.0.0-20220906165146-f3363e06e74c // indirect
+)sys12
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/tabish-bp/go_db_app/models"
+	"github.com/tabish-bp/go_db_app/storage"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) CreateBook(c *fiber.Ctx) error {
+	book := Book{}
+
+	err := c.BodyParser(&book)
+
+	if err != nil {
+		c.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"status": "error", "message": "request failed"})
+		return err
+	}
+
+	err = r.DB.Create(&book).Error
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"status": "error", "message": "book not created"})
+
+		return err
+	}
+	c.Status(http.StatusCreated).JSON(
+		&fiber.Map{"status": "success", "message": "book created"})
+
+	return nil
+}
+
+func (r *Repository) GetBooks(c *fiber.Ctx) error {
+	bookModels := &[]models.Book{}
+
+	err := r.DB.Find(&bookModels).Error
+
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"status": "error", "message": "could not get books"})
+		return err
+	}
+	c.Status(http.StatusOK).JSON(
+		&fiber.Map{"status": "success", "message": "books retrieved", "data": bookModels})
+	return nil
+}
+
+func (r *Repository) GetBookByID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	bookModel := &models.Book{}
+
+	if id == "" {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "id cannot be empty"})
+		return nil
+	}
+
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
+	if err != nil {
+		c.Status(http.StatusNotFound).JSON(
+			&fiber.Map{"message": "could not find book"})
+		return err
+	}
+
+	c.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "book retrieved", "data": bookModel})
+	return nil
+}
+
+func (r *Repository) DeleteBook(c *fiber.Ctx) error {
+	bookModel := &[]models.Book{}
+	id := c.Params("id")
+
+	if id == "" {
+		c.Status(http.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "id cannot be empty"})
+	}
+
+	err := r.DB.Delete(bookModel, id)
+	if err.Error != nil {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not delete book"})
+		return err.Error
+	}
+
+	c.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "book deleted"})
+	return nil
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_book", r.CreateBook)
+	api.Delete("/delete_book/:id", r.DeleteBook)
+	api.Get("/get_book/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Database: os.Getenv("DB_DATABASE"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = models.MigrateBooks(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := Repository{
+		DB: db,
+	}
+
+	app := fiber.New()
+
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+
+}
